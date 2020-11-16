87d20ef06fc2fe5748445cdec7e9e379c3c77e15
KYLE DOHERTY <kyledoherty@KYLEs-iMac-Pro.local>
2020-11-16 08:07:16 -0700

    first commit

---
+++ app.R
@@ -0,0 +1,121 @@
+library(shiny)
+library(tidyverse)
+library(stringr)
+
+baseDir <- '~/mpgPostdoc/projects/plantID/gh_data/'
+obsDir <- paste0(baseDir, 'observations/')
+if(!dir.exists(obsDir)){dir.create(obsDir)}
+
+full_grid <- read.csv(paste0(baseDir,'photo_layout.csv'))
+layoutPts <- read.csv(paste0(baseDir,'pts_layout.csv'))
+
+full_grid$ind <- str_pad(full_grid$ind, 4, pad = '0') 
+
+plotLayout <- function(pset, objective){
+  colnames(layoutPts)[3] <- objective
+  pDat <- left_join(layoutPts %>% as.data.frame(),
+                    full_grid %>% 
+                      filter(photo_set == pset),
+                    by = objective) 
+  pDat[is.na(pDat)] <- 0
+  
+  ggplot(pDat, aes(x = x, y = y, label = ifelse(species != 0, paste(ind, toupper(species), month, sep='\n'), ''))) +
+    geom_point(size = 20) +
+    theme_void() +
+    geom_label( fontface = "bold") +
+    coord_cartesian(xlim = c(min(pDat$x) - 90, max(pDat$x) + 90),
+                    ylim = c(min(pDat$y) - 36, max(pDat$y) + 36)) 
+}
+
+
+ui <- fluidPage(sidebarLayout(
+  sidebarPanel(
+    verbatimTextOutput('file_check'),
+    selectInput(
+      "photo_set",
+      label = "Photo Set",
+      choices = unique(full_grid$photo_set)
+    ),
+    radioButtons(
+      "objective",
+      label = "Objective",
+      choices = c('center_pos', 'corner_pos', 'random_pos')
+    ),
+    checkboxGroupInput('flower_check', 'Which flowering:', choices = NULL),
+    actionButton("stamp_time", "Stamp time")
+  ),
+  mainPanel(plotOutput('plot'))
+))
+
+server <-  function(input, output, session) {
+  
+  observe({input$photo_set})
+  
+  observeEvent(input$photo_set, {
+    updateRadioButtons(
+      session,
+      "objective",
+      label = "Objective",
+      choices =  c('center_pos', 'corner_pos', 'random_pos'),
+      selected = 'center_pos'
+    )
+  })
+  
+  observe({
+    x <- input$photo_set
+    
+    focalInds <- full_grid %>%
+      filter(photo_set == x) %>%
+      select(ind) %>%
+      unlist()
+    
+    updateCheckboxGroupInput(session, "flower_check", "Which flowering:",
+                             choices = paste(focalInds))
+   
+  })
+  
+  observeEvent(input$stamp_time,{
+    stamped <- stamped_data
+    format_set <- str_pad(input$photo_set, 4, pad = '0') 
+    
+    file_base <- paste0(format_set, '_', str_remove(input$objective, '_pos'), '.csv')
+    file_full <- paste(obsDir, file_base)
+    if(file.exists(file_full)){
+      showModal(modalDialog(
+        "Stamped already!"
+      ))
+    } else {
+      stamped <- data.frame(photo_set = format_set,
+                            objective = input$objective,
+                            time = Sys.time(),
+                            flower_inds = paste(input$flower_check, collapse = ' ')
+      )
+                            
+      write.csv(stamped, file_full, row.names = FALSE)
+    }
+    
+  })
+  
+  output$plot <- renderPlot({
+    plotLayout(pset = input$photo_set,
+               objective = input$objective)
+    
+   
+  })
+  
+  existing_files <- reactive({
+    input$stamp_time
+    fs <- list.files(path = obsDir, pattern = str_pad(input$photo_set, 4, pad = '0') )
+    if(length(fs) == 0) {
+      'No files!'
+    } else {
+      paste(fs, collapse = '\n')
+    }
+    
+  })
+  
+  output$file_check <- renderText({existing_files()})  
+  
+}
+
+shinyApp(ui, server)
---
+++ design_photos.R
@@ -0,0 +1,54 @@
+library(tidyverse)
+
+speices <- c('gaiari', #Gaillardia aristata
+                   'ratcol', #Ratibita columnifera
+                   'achmil', #Achilea millefolium
+                   'psespi', #Pseudoroegneria spicata
+                   'poasec', #Poa seconda
+                   'elytra', #Elymus trachycaulus
+                   'linlew', #Linum lewisii
+                   'vendub', #Venenata dubia
+                   'brotec', #Bromus tectorum
+                   'centsto' #Centaurea stoebe
+                   )
+
+month <- c('april','may','june','july','august','september','november')
+
+base_grid <- expand.grid(as.character(speices), as.character(month)) %>% as.data.frame()
+colnames(base_grid) <- c('species','month')
+base_grid$species <- as.character(base_grid$species)
+base_grid$month <- as.character(base_grid$month)
+
+n <- 40
+full_grid_ordered <- do.call("rbind", replicate(n, base_grid, simplify = FALSE))
+
+plants_in_set <- 4
+photos_set_seq <- 1:(nrow(full_grid_ordered)/plants_in_set)
+
+layout <- raster(nrow = 5, ncol =4)
+values(layout) <- 1:ncell(layout)
+center_cells <- c(6,7,10,11)
+corner_cells <- c(1,4,17,20)
+
+photo_set = unlist(lapply(FUN = function(x){rep(x, plants_in_set)}, X = photos_set_seq))
+centered_pos = rep(center_cells, length(photos_set_seq))
+corner_pos = rep(corner_cells, length(photos_set_seq))
+layoutPts <- rasterToPoints(layout) %>% as.data.frame()
+
+set.seed(123)
+full_grid <- full_grid_ordered %>% 
+  sample_frac(1) %>% 
+  mutate(ind = row_number(),
+         photo_set = photo_set,
+         center_pos = centered_pos,
+         corner_pos = corner_pos) %>% 
+  group_by(photo_set) %>%
+  mutate(random_pos = sample(1:20, size = 4, replace = FALSE)) %>%
+  ungroup()
+
+write.csv(full_grid, '~/mpgPostdoc/projects/plantID/gh_data/photo_layout.csv', row.names = FALSE)
+write.csv(layoutPts, '~/mpgPostdoc/projects/plantID/gh_data/pts_layout.csv', row.names = FALSE)
+
+  
+  
+  
